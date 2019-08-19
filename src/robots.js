4c97c0fbbe325d230ffa9d736c71a8b93155cf4a
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-08-18 21:40:21 -0500

    created mock data set, displayed 3 user infomations or cards, and distributing data via props

---
+++ src/robots.js
@@ -0,0 +1,6 @@
+export const robots = [
+	{ id: 1, name: 'Leanne', username: 'Bret', email: 'Sincere@april.biz' },
+	{ id: 2, name: 'Jane', usename: 'Antonette', email: 'sanna@mellissa.tv' },
+	{ id: 3, name: 'Joe', usename: 'pipper', email: 'piper@gmail.com' },
+	{ id: 4, name: 'Jack', username: 'jackinthebox', email: 'jack@gmail.com' }
+];
