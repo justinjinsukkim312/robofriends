14f281b2406326ca050251684ccf45a4c9d86320
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-08-22 15:47:31 -0500

    brought filtered robots into scope for cardlist

--- src/App.js
+++ src/App.js
@@ -15,20 +15,19 @@ class App extends React.Component {
 
 	onSearchChange = event => {
 		this.setState({ searchfield: event.target.value });
+	};
+
+	render() {
 		const filteredRobots = this.state.robots.filter(robots => {
 			return robots.name
 				.toLowerCase()
 				.includes(this.state.searchfield.toLowerCase());
 		});
-		console.log(filteredRobots);
-	};
-
-	render() {
 		return (
 			<div className="tc">
 				<h1>RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange} />
-				<CardList robots={this.state.robots} />
+				<CardList robots={filteredRobots} />
 			</div>
 		);
 	}
