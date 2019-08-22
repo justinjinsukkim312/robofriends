08d581ac2508732ebb75144f97a70bcf05c2c404
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-08-22 15:45:52 -0500

    filtered robots

--- src/robots.js
+++ src/robots.js
@@ -2,5 +2,21 @@ export const robots = [
 	{ id: 1, name: 'Leanne', username: 'Bret', email: 'Sincere@april.biz' },
 	{ id: 2, name: 'Jane', usename: 'Antonette', email: 'sanna@mellissa.tv' },
 	{ id: 3, name: 'Joe', usename: 'pipper', email: 'piper@gmail.com' },
-	{ id: 4, name: 'Jack', username: 'jackinthebox', email: 'jack@gmail.com' }
+	{ id: 4, name: 'Jack', username: 'jackinthebox', email: 'jack@gmail.com' },
+	{ id: 5, name: 'Loren', username: 'Brand', email: 'lorensupercar@cars.com' },
+	{
+		id: 6,
+		name: 'Casandra',
+		usename: 'XWoman',
+		email: 'cassaroonie@gmail.com'
+	},
+	{ id: 7, name: 'Rebecka', usename: 'Becca', email: 'popluck@gmail.com' },
+	{
+		id: 8,
+		name: 'John',
+		username: 'johnnycash',
+		email: 'johnnyappleseed@gmail.com'
+	},
+	{ id: 9, name: 'Jordan', username: '23man', email: 'thegoat@gmail.com' },
+	{ id: 10, name: 'Jonas', usename: 'thesinger', email: 'thesinger@songs.tv' }
 ];
