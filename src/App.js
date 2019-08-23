bca58f65f87949532bab895792878ae47b301c46
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-08-22 22:11:30 -0500

    added scroll feature

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import Scroll from './Scroll';
 import './App.css';
+// class syntax bc, using state
 class App extends React.Component {
 	constructor() {
-		// calls the constructor of component
 		super();
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		};
+	}
+	// lifecycle method - mounting
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => this.setState({ robots: users }));
 	}
 
 	onSearchChange = event => {
@@ -23,13 +29,19 @@ class App extends React.Component {
 				.toLowerCase()
 				.includes(this.state.searchfield.toLowerCase());
 		});
-		return (
-			<div className="tc">
-				<h1 className="f1">RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange} />
-				<CardList robots={filteredRobots} />
-			</div>
-		);
+		if (this.state.robots.length === 0) {
+			return <h1>Loading</h1>;
+		} else {
+			return (
+				<div className="tc">
+					<h1 className="f1">RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange} />
+					<Scroll>
+						<CardList robots={filteredRobots} />
+					</Scroll>
+				</div>
+			);
+		}
 	}
 }
 
