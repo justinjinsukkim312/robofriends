de0d6ebbf4651a26fb6cbf0f894dec37361b7efb
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-08-23 14:58:22 -0500

    create an action that gets dispatched into a reducer and changes the state in the store and react listens to those changes and update the view

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
+
+import { setSearchField } from '../actions';
+
 import '../App.css';
+
+const mapStatetoProps = state => {
+	return {
+		searchField: state.searchField
+	};
+};
+
+const mapDispatchToProps = dispatch => {
+	return {
+		onSearchChange: event => dispatch(setSearchField(event.target.value))
+	};
+};
+
 // class syntax bc, using state
 class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			robots: [],
-			searchfield: ''
+			robots: []
 		};
 	}
 	// lifecycle method - mounting
@@ -20,21 +36,18 @@ class App extends React.Component {
 			.then(users => this.setState({ robots: users }));
 	}
 
-	onSearchChange = event => {
-		this.setState({ searchfield: event.target.value });
-	};
-
 	render() {
-		const { robots, searchfield } = this.state;
+		const { robots } = this.state;
+		const { searchField, onSearchChange } = this.props;
 		const filteredRobots = robots.filter(robot => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 		});
 		return !robots.length ? (
 			<h1>Loading</h1>
 		) : (
 			<div className="tc">
 				<h1 className="f1">RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange} />
+				<SearchBox searchChange={onSearchChange} />
 				<Scroll>
 					<ErrorBoundary>
 						<CardList robots={filteredRobots} />
@@ -44,5 +57,9 @@ class App extends React.Component {
 		);
 	}
 }
-
-export default App;
+// connect's higher order component syntax
+// subscribe to any state changes
+export default connect(
+	mapStatetoProps,
+	mapDispatchToProps
+)(App);
